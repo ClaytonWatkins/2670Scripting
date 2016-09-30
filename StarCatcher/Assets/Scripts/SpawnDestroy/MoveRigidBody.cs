9abafef65353f11143f15e48cde84edbedf062ca
Anthony Romrell <anthony@artisandream.com>
2016-09-29 22:27:27 -0600

    Finished Feature, added VS Code reference.

---
+++ StarCatcher/Assets/Scripts/SpawnDestroy/MoveRigidBody.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRigidBody : MonoBehaviour {
+
+	// Use this for initialization
+	private Rigidbody rb;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(Vector3.right*300);
+	}
+	
+}
