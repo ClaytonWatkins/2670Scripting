dce7b70031a7ef30d65d47570d72f33110f67fb0
Anthony Romrell <anthony@artisandream.com>
2016-09-29 21:45:48 -0600

    Asset Update

---
+++ StarCatcher/Assets/Scripts/SeniorProjects/GirlRange.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GirlRange : MonoBehaviour {
+
+    public static Action<Transform> AddGirl;
+
+	// Use this for initialization
+	void Start () {
+        AddGirl(transform);
+	}
+
+}
