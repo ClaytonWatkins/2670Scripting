334912cbb28a3730986a47d6c078aed90bcbdde6
Anthony Romrell <anthony@artisandream.com>
2016-10-17 15:29:33 -0600

    Work. Yo.

---
+++ StarCatcher/Assets/Scripts/ThingsNeverToDo/FindThingsRight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FindThingsRight : MonoBehaviour {
+
+    public List<GameObject> level;
+
+    void AddToList (GameObject _go)
+    {
+        level.Add(_go);
+    }
+
+    void Start()
+    {
+        SendThisRight.SendThis += AddToList;
+    }
+}
---
+++ StarCatcher/Assets/Scripts/ThingsNeverToDo/SendThisRight.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SendThisRight : MonoBehaviour {
+
+    public static Action<GameObject> SendThis;
+
+    void Start () {
+        if(SendThis != null)
+             SendThis(gameObject);
+	}
+}
