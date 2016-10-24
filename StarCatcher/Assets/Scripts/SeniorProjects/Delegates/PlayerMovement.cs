99cb471a2dfcce4c2b7d69f3472db0c592965c11
Anthony Romrell <anthony@artisandream.com>
2016-10-24 14:02:47 -0600

    Star Spawning

--- StarCatcher/Assets/Scripts/SeniorProjects/Delegates/PlayerMovement.cs
+++ StarCatcher/Assets/Scripts/SeniorProjects/Delegates/PlayerMovement.cs
@@ -6,36 +6,15 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 moveDirection;
     public float speed = 10;
-    public float gravity = 3;
+    public float gravity = 1;
     public float jumpSpeed = 30;
     CharacterController cc;
 
-    public void StopMoving()
-    {
-        MoveUsingArrowKeys.MoveOnArrows -= Move;
-        MoveUsingArrowKeys.RotateOnArrows -= Rotate;
-    }
-
-    void StopScript ()
-    {
-        MoveUsingArrowKeys.MoveOnArrows -= Move;
-        MoveUsingArrowKeys.RotateOnArrows -= Rotate;
-    }
-
-    void StartGameHandler ()
-    {
-        MoveUsingArrowKeys.MoveOnArrows += Move;
-        GameControl.StartGame -= StartGameHandler;
-    }
-
     void Start()
     {
-        EndGame.GameOver += StopScript;
         cc = GetComponent<CharacterController>();
-        GameControl.StartGame += StartGameHandler;
-       // MoveUsingButtons.Forward += Move;
-        //MoveUsingArrowKeys.RotateOnArrows += Rotate;
-        //MoveUsingButtons.Rotate += Rotate;
+        MoveUsingArrowKeys.MoveOnArrows += Move;
+        MoveUsingArrowKeys.RotateOnArrows += Rotate;
     }
 
     void Rotate(float _rotateInY)
@@ -43,12 +22,19 @@ public class PlayerMovement : MonoBehaviour
         transform.Rotate(0, _rotateInY, 0);
     }
 
-
     void Move(float _moveInZ)
     {
-        moveDirection.y -= gravity;
-        moveDirection.x = _moveInZ;
-        moveDirection = transform.TransformDirection(moveDirection * speed * Time.deltaTime);
-        cc.Move(moveDirection);
+        if (cc.isGrounded) {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                moveDirection.y = jumpSpeed * Time.deltaTime;
+            }
+
+            moveDirection.y -= gravity * Time.deltaTime;
+
+            moveDirection.x = _moveInZ * speed * Time.deltaTime;
+            moveDirection = transform.TransformDirection(moveDirection*Time.deltaTime);
+            cc.Move(moveDirection);
+        }
     }
 }
---
+++ StarCatcher/Assets/Scripts/Stars/DestroyStar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyStar : MonoBehaviour {
+
+    public float endTime = 3;
+
+	void OnCollisionEnter () {
+        Destroy(gameObject, endTime);
+    }
+
+    //IEnumerator EndStar()
+    //{
+    //    yield return new WaitForSeconds(endTime);
+    //}
+}
